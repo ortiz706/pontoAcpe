4ba6ccab5469e8a0ed79f3b6d7c58404d830dcc6
ortiz <viniciusfontes@cpejr.com.br>
2020-06-09 14:19:11 -0300

    correcao teste compilar

--- routes/users.js
+++ routes/users.js
@@ -35,6 +35,7 @@ router.get('/', auth.isAuthenticated, auth.isAdmin, (req, res) => {
  */
 router.get('/new', auth.isAuthenticated, auth.isAdmin, (req, res) => {
   Role.getAll().then((roles) => {
+    const Mandatory = require('../functions/mandatory');
     const Sheets = require('../functions/sheets');
     Mandatory.getLastWeekReport().then((report) => {
       Sheets.writeReportGoogleSheets(report);
